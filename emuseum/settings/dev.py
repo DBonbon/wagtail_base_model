6f6e468768d72551f25aacec36974e04a433057c
Yariv Friedman <yrfriedman@gmail.com>
2021-08-20 12:54:46 +0200

    initial commit

---
+++ emuseum/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!f=vqbkr_r=4*$*mqrz#&js-ks72er5h9py$3g1qn556$%cf)2'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
